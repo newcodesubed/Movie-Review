b90ad501e098233d9ee6b33ebfb5d3d279f28e1e
newcodesubed <subedshahxi@gmail.com>
2025-11-20 08:38:47 +0545

    feat: route setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 export default function App() {
   return (
-    <div>
-      <h1>Welcome to MovieApp</h1>
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route index element={<h1>Welcome to MovieApp</h1>} />
+          <Route path="movie/:id" element={<h1>Movie Detail page</h1>} />
+          <Route path="movies/:type" element={<h1>Movie List page</h1>} />
+          <Route path="movie/favorite" element={<h1>Favorite Movies</h1>} />
+          <Route path="/*" element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
